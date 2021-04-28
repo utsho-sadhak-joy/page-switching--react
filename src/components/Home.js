541f558f7e6e822dbd212696458a47c74b44255d
Joy <utsho233@gmail.com>
2021-04-29 00:14:29 +0600

    differentiate different component through color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Sadia from './Sadia';
+import Puja from './Puja';
+
 
 const Home = () => {
     return (
-        <div>
+        <div style={{backgroundColor:"red"}}>
             <h1>Hi sexy</h1>
+            <Sadia />
+            <Puja />
         </div>
     )
 }
---
+++ src/components/Sadia.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sadia = () => {
+    return (
+        <div style={{backgroundColor:"yellow"}}>
+            <h1>My name is Sadia</h1>
+        </div>
+    )
+}
+
+export default Sadia
