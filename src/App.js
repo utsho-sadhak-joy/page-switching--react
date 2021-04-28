024e08708957be33a6afac05c0959dcd645a2e94
Joy <utsho233@gmail.com>
2021-04-29 00:04:12 +0600

    Showing components through app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>I love my work</h1>
+      <Home />
     </div>
   );
 }
