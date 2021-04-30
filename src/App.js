172225a923ae885a0253b6d4b31a145fe95b6103
Joy <utsho233@gmail.com>
2021-04-30 23:03:33 +0600

    make new environment for my next app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, {useState} from 'react';
-import Card from './components/Card';
-import Puja from './components/Puja';
-import {Container} from 'react-bootstrap'
+import React from 'react';
+import Home from './components/Home';
 
 
 
 function App() {
-  const[active, setActive] = useState("firstCard");
   return (
     <div className="App">
-      <nav>
-        <button onClick={()=> setActive("firstCard")}>one</button>
-        <button onClick={()=> setActive("secondCard")}>two</button>
-        <button onClick={()=> setActive("thirdCard")}>three</button>
-      </nav>
-      <Container>
-        {active === "firstCard" && <Card title="p" />}
-        {active === "secondCard" && <Card title="2" />}
-        {active === "thirdCard" && <Puja title="3" />}
-        
-      </Container>
+      <Home />
       
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,11 @@
-import React from 'react'
-import Sadia from './Sadia';
-import Puja from './Puja';
-import Sathy from './Sathy';
-import Button from 'react-bootstrap/Button';
-
-
-
-
-const  sexy = () => {
-    console.log("Babu Khaicho");
-}
-
-
-
+import React from 'react';
 
 
 
 const Home = () => {
     return (
-        <div style={{backgroundColor:"black",height:"350px"}}>
-            {/* <h1>Hi sexy</h1>s */}
-            {/* <Sadia />
-            <Puja /> */}
-
-               <Sadia />
-               <Puja />
-               <Sathy />
-
-            <Button className="btn btn-secondary ml-5 mt-5">Sadia</Button>
-            <Button className="btn btn-secondary ml-5 mt-5" onClick={sexy} >Puja</Button>
-            {/* <button onClick={this.handleClick()}>Click Me</button> */}
-            {/* onClick='window.alert("I wanna fuck you!") */}
+        <div>
+            <h1>Utsho Sadhak Joy</h1>
         </div>
     )
 }
